d7358c8d957e9ea25fa93a0cf9b67429c4628339
KvasKupecheskiy <tdaneshi@mail.ru>
2023-03-07 15:14:41 +0600

    Update

---
+++ tsis2/Lists.py
@@ -0,0 +1,11 @@
+thislist = ["apple", "banana", "cherry"]
+print (thislist)
+print (len(thislist))
+list1 = ["apple", "banana", "cherry"]
+list2 = [1, 5, 7, 9, 3]
+list3 = [True, False, False]
+list1 = ["abc", 34, True, 40, "male"]
+mylist = ["apple", "banana", "cherry"]
+print(type(mylist))
+thislist = list(("apple", "banana", "cherry")) # note the douple brackets
+print (thislist)
