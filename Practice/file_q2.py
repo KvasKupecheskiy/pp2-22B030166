c944a9fb5bca456c67511314a6dbe32fbf1c3c64
KvasKupecheskiy <123073160+KvasKupecheskiy@users.noreply.github.com>
2023-04-13 13:52:26 +0600

    little cnange

--- Practice/file_q2.py
+++
@@ -1,8 +0,0 @@
-import os
-import shutil
-
-path = os.getcwd()
-
-#os.listdir -
-for file in os.listdir(path):
-    print(file)
--- Practice/gen_q2.py
+++
@@ -1,8 +0,0 @@
-def even_numbers():
-    number = 2
-    for i in range(n):
-      if i % 2 == 0:
-        yield i
-    while True:
-       yield number
-       number += 2
